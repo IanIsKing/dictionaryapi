70a41a105b2e56f8c85af727555b331d641d7953
IanIsKing <ian@ecornice.co.za>
2024-01-11 18:51:42 +0200

    removing api key

--- src/Fetch.test.js
+++ src/Fetch.test.js
@@ -4,9 +4,7 @@ import { fetchWordDefinition } from "./fetchWordDefinition";
 import { expect, test } from "@jest/globals";
 
 test("fetch Word Definition", async () => {
-  const data = await fetchWordDefinition(
-    "test",
-    "3cc41a21-ea1b-49e0-a6c8-cd0b5a1723ca"
-  );
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const data = await fetchWordDefinition("test", apiKey);
   expect(data[0].hwi.hw).toContain("test");
 });
